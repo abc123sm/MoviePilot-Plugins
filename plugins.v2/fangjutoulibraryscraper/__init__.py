2d859781c7a348e75564ec425cdf48d995de27a4
abc123sm <zaqw6414@gmail.com>
2025-02-25 15:51:58 +0000

    Add files via upload

--- plugins.v2/fangjutoulibraryscraper/__init__.py
+++ plugins.v2/fangjutoulibraryscraper/__init__.py
@@ -35,7 +35,7 @@ class fangjutouLibraryScraper(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "fangjutoulibraryscraper_"
     # 加载顺序
-    plugin_order = 7
+    plugin_order = 1
     # 可使用的用户级别
     user_level = 1
 
