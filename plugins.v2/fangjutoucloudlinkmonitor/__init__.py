828dcd019828622c89b29f0e39d40937fcc032b6
abc123sm <zaqw6414@gmail.com>
2025-03-05 06:45:44 +0000

    Add files via upload

--- plugins.v2/fangjutoucloudlinkmonitor/__init__.py
+++ plugins.v2/fangjutoucloudlinkmonitor/__init__.py
@@ -63,7 +63,7 @@ class FangjutouCloudLinkMonitor(_PluginBase):
     # 插件图标
     plugin_icon = "Linkease_A.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "abc123sm"
     # 作者主页
